9c1f981a37cff9313c3ec96e464662002c1e5b07
rishyak <hello@rishyak.com>
2023-01-19 01:02:07 -0500

    Move prompts into prompts.py
    Signed-off-by: rishyak <hello@rishyak.com>

--- RockPaperScissors/game.py
+++ RockPaperScissors/game.py
@@ -1,53 +1,7 @@
 import os
 from random import choice
 from RockPaperScissors.scoreboard import Scoreboard
-
-start = """Welcome to Rock Paper Scissors!
-
-Rock Paper Scissors is a simple game played between two players. 
-
-It has three possible outcomes: Win, Loss, Draw.
-
-The rules are quite simple. 
-
-Press Enter to continue. 
-"""
-
-rules = """Both players make a choice and reveal their choices simultaneously.
-
-All you need to remember is:
-    - Rock beats Scissors
-    - Paper beats Rock
-    - Scissors beat Paper
-
-The player with the winning object is awarded one point.
-
-If both players play the same object, such as rock-rock, then it's a tie. 
-No points are awarded in case of a tie.
-
-Press Enter to continue.
-"""
-
-how_to = """Controls:
-    - 1: Rock
-    - 2: Paper
-    - 3: Scissors
-    - Q/q: Quit
-
-To confirm your choice after selecting, press "Enter" or "Return".
-
-Press Enter to start. Good luck!
-"""
-
-game_prompt = """Make your selection.
-1: Rock
-2: Paper
-3: Scissors
-Q/q: Quit
-
-To confirm your choice after selecting, press "Enter" or "Return".
-
-"""
+from RockPaperScissors.prompts import start, rules, how_to, game_prompt
 
 
 def clear_screen() -> None:
